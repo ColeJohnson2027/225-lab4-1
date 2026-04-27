c32cf7956921b1af8fa028338859fe2a7d51f215
ColeJohnson2027 <john2027@miamioh.edu>
2026-04-26 23:36:59 -0400

    Update testing_app.py

--- testing/testing_app.py
+++ testing/testing_app.py
@@ -1,1 +1,16 @@
+from main import app, init_db
+import main
 
+def test_home_page_loads(tmp_path):
+
+    main.DATABASE = str(tmp_path / "test.db")
+
+    app.config["TESTING"] = True
+
+    init_db()
+
+    client = app.test_client()
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert b"Add Contact" in response.data
